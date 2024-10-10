0d7eda5549592cad8f3ae1a1494c5b7862fb23ea
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2024-10-11 01:17:58 +0300

    Done The Sixth Task

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,17 @@
+/**
+ * Creates a buffer array with a given position set to a given value.
+ * @param {Number} length - The length of the buffer.
+ * @param {Number} position - The position to modify.
+ * @param {Number} value - The value to be stored in the position.
+ * @returns {DataView}
+ */
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const res = new DataView(buffer);
+  try {
+    res.setUint8(position, value);
+  } catch (error) {
+    if (error instanceof RangeError) throw new Error('Position outside range');
+  }
+  return res;
+}
